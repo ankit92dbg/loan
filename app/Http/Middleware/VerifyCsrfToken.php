96fb334ba09942f98665ede9ad4cdedde4a16412
Ankit <ankit.srivastav@4thpointer.com>
2020-02-02 14:03:50 +0530

    application

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,7 +19,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'register',
+        'registerUser',
         'getUser',
         'loanAmount',
         'newLoan',
